a9ec720c8486f0ca4ffd175da3c290ffd0c36524
Niya <disma1589@gmail.com>
2022-07-14 20:42:17 +0200

    add Icon component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Button } from "./components/Button";
+import { Icon } from "./components/Icon";
 
 function App() {
   return (
     <div>
       <Button>Join</Button>
+      <Button>
+        <Icon />
+      </Button>
     </div>
   );
 }
