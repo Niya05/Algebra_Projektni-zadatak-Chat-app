6b2b06b901c1a8cc5a3f988eb628d265a6b54956
Niya <disma1589@gmail.com>
2022-07-14 21:00:25 +0200

    add Button component

--- src/components/Button/Button.component.jsx
+++ src/components/Button/Button.component.jsx
@@ -1,7 +1,8 @@
 import "./Button.styles.scss";
 
 export function Button(props) {
-    const className=`button button--${props.variant}`
+    const className=`button button--${props.variant}`;
+    
     return (
         <button className={className}>{props.children}</button>
     )
