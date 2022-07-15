5d344eb561aa56c07660a9864e364cf4d58d30ed
Niya <disma1589@gmail.com>
2022-07-15 19:35:58 +0200

    fixed JoinPage component and style

--- src/pages/JoinPage/JoinPage.component.jsx
+++ src/pages/JoinPage/JoinPage.component.jsx
@@ -3,10 +3,14 @@ import "./JoinPage.styles.scss";
 import { JoinForm } from "../../components/JoinForm";
 
 export function JoinPage(props) {
-    return (
-        <div className="join-page">
-            <div className="join-page__title">Chat with friends</div>
-            <div className="join-page__form"><JoinForm /></div>
-        </div>
-    )
+  return (
+    <div className="join-page">
+      <div className="join-page__title">
+        Chat with friends
+      </div>
+      <div className="join-page__form">
+        <JoinForm />
+      </div>
+    </div>
+  );
 }
