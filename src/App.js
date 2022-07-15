07b8c3679b4a83a1c56028aad0c520f2fb5463bd
Niya <disma1589@gmail.com>
2022-07-15 20:40:41 +0200

    add avatar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { Avatar } from "./components/Avatar";
 import { MessageText } from "./components/MessageText";
 
 function App() {
   return (
-    <MessageText displayName="Domagoj" time="20:13">
-      Ready for the trip? I'm so excited I couldn't sleep!
-    </MessageText>
+    <div>
+      <Avatar text="AF" backgroundColor="lime" />
+      <MessageText displayName="Niya" time="20:13">
+        Ready for the trip? I'm so excited I couldn't sleep!
+      </MessageText>
+    </div>
   );
 }
 
